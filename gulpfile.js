ac98b0866b2cf124e85934ea9e4df63c5bff48bb
menor <josemenor@gmail.com>
2014-05-29 09:37:36 +0200

    Update gulp to use bourbon and neat as node modules.

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+// Include gulp
+gulp = require( 'gulp' );
+
+// Include plugins
+var jshint = require( 'gulp-jshint' ),
+    sass = require( 'gulp-sass' ),
+    concat = require( 'gulp-concat' ),
+    uglify = require( 'gulp-uglify' ),
+    rename = require( 'gulp-rename'),
+    neat = require( 'node-neat' ).includePaths;
+
+// Lint task
+gulp.task( 'lint', function() {
+  return gulp.src( 'js/*.js' )
+    .pipe( jshint() )
+    .pipe( jshint.reporter( 'default' ));
+});
+
+// Compile sass
+gulp.task( 'sass', function() {
+  return gulp.src('scss/*.scss')
+    .pipe( sass({
+      includePaths: ['sass'].concat( neat )
+    }))
+    .pipe( gulp.dest( 'css' ) );
+});
+
+// Concatenate & minify JS
+gulp.task( 'scripts', function() {
+    return gulp.src( 'js/*.js' )
+      .pipe( concat( 'all.js' ) )
+      .pipe( gulp.dest( 'dist' ) )
+      .pipe( rename( 'all.min.js' ) )
+      .pipe( uglify() )
+      .pipe( gulp.dest( 'dist' ) );
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+  gulp.watch( 'js/*.js', ['lint', 'scripts'] );
+  gulp.watch( 'scss/*.scss', ['sass'] );
+});
+
+// Default Task
+gulp.task( 'default', ['lint', 'sass', 'scripts', 'watch'] );
